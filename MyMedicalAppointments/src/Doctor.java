d359300d3e53ce1b34720e4803e02e04f4dc4465
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-20 21:11:12 -0500

    My first commit

---
+++ MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor {
+
+    // Atributos
+    int id;
+    String name;
+    String speciality;
+
+    // Comportamientos
+
+    // Imprime el nombre de la persona
+    public void showName(){
+        System.out.println(name);
+    }
+
+}
