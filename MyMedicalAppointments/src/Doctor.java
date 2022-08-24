6034080266ee8fd9b33222e29250b69cd4b46eec
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-08-23 21:28:55 -0500

    Aplicando herencia en Java

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -2,37 +2,19 @@ import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
+public class Doctor extends User{
 
     // Atributos
-    static int id = 0; // Auto-Incrementado
-    private String name;
-    private String email;
     private String speciality;
 
     // Declarando el método constructor
-    public Doctor(){
-        System.out.println("Creando el objeto Doctor");
-        id ++;
-    }
-
-    // Metodo constructor sobrecargado
-    public Doctor(String name, String speciality){
-        this.name = name;
-        this.speciality = speciality;
+    public Doctor(String name, String email){
+        super(name, email);
         System.out.println("Nombre del Dcotor asignado es: " + name);
-        id++;
     }
 
     // Comportamientos
-    // Imprime el nombre de la persona
-    public void showName(){
-        System.out.println(name);
-    }
-    //Imprime el ID del doctor;
-    public void showId(){
-        System.out.println("ID Doctor: " + id);
-    }
+
     //Añade una cita
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
     public void addAvailableAppointment(Date date, String time){
@@ -43,6 +25,14 @@ public class Doctor {
         return availableAppointments;
     }
 
+    // Getter y Setters
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
 
     //Clases Anidada
     public static class AvailableAppointment{
